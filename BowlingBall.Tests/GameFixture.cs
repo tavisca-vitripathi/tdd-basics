db925cb2dd09a1a1adafcc455f2161404b8d6997
tavisca-vitripathi <tripathi.t@yahoo.com>
2019-07-10 19:55:55 +0530

    Update GameFixture.cs

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,6 +5,50 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
+         private Game g;
+
+        [Fact]
+        public void allNull()
+        {
+            g = new Game();
+            rollTheBall(20, 0);
+            Assert.Equal(0, g.getScore());
+        }
+        [Fact]
+        public void checkSpare()
+        {
+            g = new Game();
+            g.Roll(7);
+            g.Roll(3);
+            g.Roll(5);
+            rollTheBall(17, 0);
+            Assert.Equal(20, g.getScore());
+        }
+
+        [Fact]
+        public void checkStrike()
+        {
+            g = new Game();
+            g.Roll(10);
+            g.Roll(3);
+            g.Roll(5);
+            rollTheBall(16, 0);
+            Assert.Equal(26, g.getScore());
+        }
+        [Fact]
+        public void allStrike()
+        {   g = new Game();
+            rollTheBall(12,10);
+            Assert.Equal(300, g.getScore());
+        }
+        private void rollTheBall(int times, int score)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                g.Roll(score);
+            }
+        }
+   
         [Fact]
         public void DummyTest()
         {
